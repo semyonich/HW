05018b95b120671330ac4dc7f184e7b938a89622
semyonich <s.semenihin@gmail.com>
2017-06-14 20:15:19 +0300

    hw 08.06 final

--- hw_08.06.py
+++ hw_08.06.py
@@ -1,38 +1,35 @@
 my_dict = {
-    "first_name" : "I-vann",
-    "last_name" : "McCloud",
-    "email" : "vano@gmail.com",
-    "age" : 26
+    "first_name": "I-vann",
+    "last_name": "Cllaud",
+    "email": "vano@gmail.com",
+    "age": 26
 }
 print("Step 2 result is:", my_dict)
 new_list = []
+new_filtered_list = []
 
 my_list = my_dict.values()
 print("Step 3 before processing:", my_list)
 
-for itm in my_list:
-    if isinstance(itm,str) and len(itm) > 5 and itm.find("a") != -1:
-        new_list.append(itm)
-    elif isinstance(itm,int) and itm > 25 and itm < 40:
-        itm = str(itm)
-        new_list.append(itm)
-    # else :
+for value in my_list:
+    if isinstance(value, str) and len(value) > 5 and value.find("a") >= 0:
+        new_list.append(value)
+    elif isinstance(value, int) and 40 > value > 25:
+        # value =
+        new_list.append(str(value))
 print("Step 3 result is:", new_list)
 
-# Here must be logical sequence, but i haven't written it
-# for itm in new_list:
-#     if isinstance(itm,str):
-#         if itm.find("m") != -1 or itm.find("n") != -1:
-#             new_list.remove(itm)
-# print("Step 4 result is:", new_list)
-
-new_list.sort(reverse=True)
-print("Step 5 result is:", new_list)
-
-new_str = ""
-for itm in new_list: new_str += itm + ","
-print("Step 6 result is:", new_str, type(new_str))
 
 for itm in new_list:
-    itm = itm.split(",")
+    if "n" not in itm and "m" not in itm:
+        new_filtered_list.append(itm)
+print("Step 4 result is:", new_filtered_list, type(new_filtered_list))
+
+new_filtered_list.sort(reverse=True)
+print("Step 5 result is:", new_filtered_list, type(new_filtered_list))
+
+new_str = ",".join(new_filtered_list)
+print("Step 6 result is:", new_str, type(new_str))
+
+new_list = new_str.split(",")
 print("Step 7 result is:", new_list, type(new_list))
